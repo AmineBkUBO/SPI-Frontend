24ccfb2410d326f80e18f0d2d673b19099a52178
AmineBkUBO <aminejustagamer@gmail.com>
2026-01-27 10:21:08 +0100

    Added routes to edit pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
+import {useState} from "react";
+import {Routes, Route, useLocation} from "react-router-dom";
+import {AnimatePresence} from "framer-motion";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
@@ -13,8 +13,8 @@ import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 import Geography from "./scenes/geography";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./theme";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import {ColorModeContext, useMode} from "./theme";
 import Calendar from "./scenes/calendar/calendar";
 import PromotionList from "./Pages/Promotion/PromotionList";
 import FormationList from "./Pages/Formation/FormationList";
@@ -31,142 +31,178 @@ import CreateEnseignantForm from "./Pages/Enseignant/CreateEnseignantForm";
 import CreateFormationForm from "./Pages/Formation/CreateFormationForm";
 import CreatePromotionForm from "./Pages/Promotion/CreatePromotionForm";
 import CreateEtudiantForm from "./Pages/Etudiant/CreateEtudiantForm";
+import EditEnseignantForm from "./Pages/Enseignant/EditEnseignantForm";
+import EditFormationForm from "./Pages/Formation/EditFormationForm";
+import EditPromotionForm from "./Pages/Promotion/EditPromotionForm";
+import EditEtudiantForm from "./Pages/Etudiant/EditEtudiantForm";
 
 function App() {
-  const [theme, colorMode] = useMode();
-  const [isSidebar, setIsSidebar] = useState(true);
-  const location = useLocation();
+    const [theme, colorMode] = useMode();
+    const [isSidebar, setIsSidebar] = useState(true);
+    const location = useLocation();
 
-  return (
-      <ColorModeContext.Provider value={colorMode}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <div className="app">
-            <Sidebar isSidebar={isSidebar} />
-            <main className="content">
-              <Topbar setIsSidebar={setIsSidebar} />
-              <AnimatePresence mode="wait">
-                <Routes location={location} key={location.pathname}>
-                  <Route path="/" element={<PageTransition><Dashboard /></PageTransition>} />
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
+                <div className="app">
+                    <Sidebar isSidebar={isSidebar}/>
+                    <main className="content">
+                        <Topbar setIsSidebar={setIsSidebar}/>
+                        <AnimatePresence mode="wait">
+                            <Routes location={location} key={location.pathname}>
+                                <Route path="/" element={<PageTransition><Dashboard/></PageTransition>}/>
 
-                  <Route
-                      path="/etudiants"
-                      element={
-                        <PageTransition>
-                          <EtudiantList title="ETUDIANTS" subtitle="Liste des étudiants :" />
-                        </PageTransition>
-                      }
-                  />
-                  <Route
-                      path="/etudiant/:slug"
-                      element={
-                        <PageTransition>
-                          <EtudiantDetail />
-                        </PageTransition>
-                      }
-                  />
-                  <Route
-                      path="/etudiant/add"
-                      element={
-                        <PageTransition>
-                          <CreateEtudiantForm />
-                        </PageTransition>
-                      }
-                  />
+                                <Route
+                                    path="/etudiants"
+                                    element={
+                                        <PageTransition>
+                                            <EtudiantList title="ETUDIANTS" subtitle="Liste des étudiants :"/>
+                                        </PageTransition>
+                                    }
+                                />
+                                <Route
+                                    path="/etudiant/:slug"
+                                    element={
+                                        <PageTransition>
+                                            <EtudiantDetail/>
+                                        </PageTransition>
+                                    }
+                                />
+                                <Route
+                                    path="/etudiant/add"
+                                    element={
+                                        <PageTransition>
+                                            <CreateEtudiantForm/>
+                                        </PageTransition>
+                                    }
+                                />
+                                <Route
+                                    path="/etudiant/edit/:slug"
+                                    element={
+                                        <PageTransition>
+                                            <EditEtudiantForm/>
+                                        </PageTransition>
+                                    }
+                                />
 
-                  <Route
-                      path="/promotions"
-                      element={
-                        <PageTransition>
-                          <PromotionList title="PROMOTIONS" subtitle="Liste des promotions :" />
-                        </PageTransition>
-                      }
-                  />
-                  <Route
-                      path="/promotion/:slug"
-                      element={
-                        <PageTransition>
-                          <PromotionDetail />
-                        </PageTransition>
-                      }
-                  />
-                  <Route
-                      path="/promotion/add"
-                      element={
-                        <PageTransition>
-                          <CreatePromotionForm />
-                        </PageTransition>
-                      }
-                  />
+                                <Route
+                                    path="/promotions"
+                                    element={
+                                        <PageTransition>
+                                            <PromotionList title="PROMOTIONS" subtitle="Liste des promotions :"/>
+                                        </PageTransition>
+                                    }
+                                />
+                                <Route
+                                    path="/promotion/:slug"
+                                    element={
+                                        <PageTransition>
+                                            <PromotionDetail/>
+                                        </PageTransition>
+                                    }
+                                />
+                                <Route
+                                    path="/promotion/add"
+                                    element={
+                                        <PageTransition>
+                                            <CreatePromotionForm/>
+                                        </PageTransition>
+                                    }
+                                />
+                                <Route
+                                    path="/promotion/edit/:slug"
+                                    element={
+                                        <PageTransition>
+                                            <EditPromotionForm/>
+                                        </PageTransition>
+                                    }
+                                />
 
-                  <Route
-                      path="/formations"
-                      element={
-                        <PageTransition>
-                          <FormationList title="FORMATIONS" subtitle="Liste des formations :" />
-                        </PageTransition>
-                      }
-                  />
-                  <Route
-                      path="/formation/:slug"
-                      element={
-                        <PageTransition>
-                          <FormationDetail />
-                        </PageTransition>
-                      }
-                  />
-                  <Route
-                    path="/formation/add"
-                    element={
-                      <PageTransition>
-                        <CreateFormationForm />
-                      </PageTransition>
-                    }
-                />
+                                <Route
+                                    path="/formations"
+                                    element={
+                                        <PageTransition>
+                                            <FormationList title="FORMATIONS" subtitle="Liste des formations :"/>
+                                        </PageTransition>
+                                    }
+                                />
+                                <Route
+                                    path="/formation/:slug"
+                                    element={
+                                        <PageTransition>
+                                            <FormationDetail/>
+                                        </PageTransition>
+                                    }
+                                />
+                                <Route
+                                    path="/formation/add"
+                                    element={
+                                        <PageTransition>
+                                            <CreateFormationForm/>
+                                        </PageTransition>
+                                    }
+                                />
+                                <Route
+                                    path="/formation/edit/:slug"
+                                    element={
+                                        <PageTransition>
+                                            <EditFormationForm/>
+                                        </PageTransition>
+                                    }
+                                />
 
-                  <Route
-                      path="/enseignants"
-                      element={
-                        <PageTransition>
-                          <EnseignantList title="ENSEIGNANTS" subtitle="Liste des enseignants :" />
-                        </PageTransition>
-                      }
-                  />
-                  <Route
-                      path="/enseignant/:slug"
-                      element={
-                        <PageTransition>
-                          <EnseignantDetail />
-                        </PageTransition>
-                      }
-                  />
-                  <Route
-                      path="/enseignant/add"
-                      element={
-                        <PageTransition>
-                          <CreateEnseignantForm />
-                        </PageTransition>
-                      }
-                  />
+                                <Route
+                                    path="/enseignants"
+                                    element={
+                                        <PageTransition>
+                                            <EnseignantList title="ENSEIGNANTS" subtitle="Liste des enseignants :"/>
+                                        </PageTransition>
+                                    }
+                                />
+                                <Route
+                                    path="/enseignant/:slug"
+                                    element={
+                                        <PageTransition>
+                                            <EnseignantDetail/>
+                                        </PageTransition>
+                                    }
+                                />
+                                <Route
+                                    path="/enseignant/add"
+                                    element={
+                                        <PageTransition>
+                                            <CreateEnseignantForm/>
+                                        </PageTransition>
+                                    }
+                                />
+                                <Route
+                                    path="/enseignant/edit/:slug"
+                                    element={
+                                        <PageTransition>
+                                            <EditEnseignantForm/>
+                                        </PageTransition>
+                                    }
+                                />
 
 
-                  <Route path="/team" element={<PageTransition><Team /></PageTransition>} />
-                  <Route path="/contacts" element={<PageTransition><Contacts /></PageTransition>} />
-                  <Route path="/invoices" element={<PageTransition><Invoices /></PageTransition>} />
-                  <Route path="/form" element={<PageTransition><Form /></PageTransition>} />
-                  <Route path="/bar" element={<PageTransition><Bar /></PageTransition>} />
-                  <Route path="/pie" element={<PageTransition><Pie /></PageTransition>} />
-                  <Route path="/line" element={<PageTransition><Line /></PageTransition>} />
-                  <Route path="/faq" element={<PageTransition><FAQ /></PageTransition>} />
-                  <Route path="/calendar" element={<PageTransition><Calendar /></PageTransition>} />
-                  <Route path="/geography" element={<PageTransition><Geography /></PageTransition>} />
-                </Routes>
-              </AnimatePresence>
-            </main>
-          </div>
-        </ThemeProvider>
-      </ColorModeContext.Provider>
-  );
+                                <Route path="/team" element={<PageTransition><Team/></PageTransition>}/>
+                                <Route path="/contacts" element={<PageTransition><Contacts/></PageTransition>}/>
+                                <Route path="/invoices" element={<PageTransition><Invoices/></PageTransition>}/>
+                                <Route path="/form" element={<PageTransition><Form/></PageTransition>}/>
+                                <Route path="/bar" element={<PageTransition><Bar/></PageTransition>}/>
+                                <Route path="/pie" element={<PageTransition><Pie/></PageTransition>}/>
+                                <Route path="/line" element={<PageTransition><Line/></PageTransition>}/>
+                                <Route path="/faq" element={<PageTransition><FAQ/></PageTransition>}/>
+                                <Route path="/calendar" element={<PageTransition><Calendar/></PageTransition>}/>
+                                <Route path="/geography" element={<PageTransition><Geography/></PageTransition>}/>
+                            </Routes>
+                        </AnimatePresence>
+                    </main>
+                </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
 }
 
 export default App;
